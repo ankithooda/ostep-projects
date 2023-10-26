0d2e29db02f80f0e366f05a9060332da2a429c5c
Ankit <ankithooda23@gmail.com>
2023-10-26 20:20:06 +0530

    processes-shell
    - Adding capability to read a file

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -5,6 +5,8 @@
 #include <limits.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define INPUT_LEN 100
 
@@ -14,10 +16,24 @@ int main(int argc, char *argv[]) {
     char *command[INPUT_LEN];   // Tokenized input
     char *saveptr;              // Internal pointer for strtok_r
     int i;                      // iterator variable
+    FILE *input_file;
+    
+    if (argc == 2) {
+        input_file = fopen(argv[1], "r+");
+    } else {
+        input_file = stdin;
+    }
     while (1) {
         // Prompt user input.
+        // Empty buffer
+        for (i = 0; i < LINE_MAX; i++) {
+            input[i] = '\0';
+        }
+        for (i = 0; i < INPUT_LEN; i++) {
+            command[i] = NULL;
+        }
         printf("> ");
-        fgets(input, LINE_MAX, stdin);
+        fgets(input, LINE_MAX, input_file);
 
         // Remove trailing newline
         input[strcspn(input, "\n")] = '\0';
@@ -30,25 +46,27 @@ int main(int argc, char *argv[]) {
             i = i + 1;
         }
 
-        // Create new process for the command.
-        int rc = fork();
+        // Run if some command has been parsed.
+        if (command[0] != NULL) {
 
-        if (rc < 0) {
-            fprintf(stderr, "Unable to create process for running the command.\n");
-        } else if (rc == 0) {
-            int exec_code = execvp(command[0], command);
-            if (exec_code < 0) {
-                fprintf(stderr, "Unable to run command.\n");
-                _exit(EXIT_FAILURE);
+            // Create new process for the command.
+            int rc = fork();
+
+            
+
+            if (rc < 0) {
+                fprintf(stderr, "Unable to create process for running the command.\n");
+            } else if (rc == 0) {
+                int exec_code = execvp(command[0], command);
+                if (exec_code < 0) {
+                    fprintf(stderr, "%d\n%d\n", exec_code, errno);
+                    fprintf(stderr, "An error has occurred\n");
+                    _exit(EXIT_FAILURE);
+                }
+            } else {
+                // Wait for child process to terminate.
+                wait(NULL);
             }
-        } else {
-            // Wait for child process to terminate.
-            wait(NULL);
-        }
-
-        // Empty buffer
-        for (i = 0; i < INPUT_LEN; i++) {
-            command[i] = NULL;
         }
     }
     return 0;
