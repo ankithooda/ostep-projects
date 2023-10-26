fe7fb55eb79c73defa09a292d5956e71e8d0b92b
Ankit <ankithooda23@gmail.com>
2023-10-26 15:29:54 +0530

    processes-shell
    - Completed implementation of the basic shell.

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -6,32 +6,28 @@
 #include <sys/wait.h>
 #include <errno.h>
 
+#define INPUT_LEN 100
+
 int main(int argc, char *argv[]) {
 
-    char input[LINE_MAX]; // input buffer
-    char *command[100];   // Tokenized input
-    char *saveptr;        // Internal pointer for strtok_r
-    int i = 1;            // iterator variable
+    char input[LINE_MAX];       // input buffer
+    char *command[INPUT_LEN];   // Tokenized input
+    char *saveptr;              // Internal pointer for strtok_r
+    int i;                      // iterator variable
     while (1) {
-        // Prompt
+        // Prompt user input.
         printf("> ");
         fgets(input, LINE_MAX, stdin);
 
+        // Remove trailing newline
+        input[strcspn(input, "\n")] = '\0';
+
         // Parse input into command and arguments.
-        command[0] = strtok_r(input, " ", &saveptr);    
+        command[0] = strtok_r(input, " ", &saveptr);
+        i = 1;  
         while (command[i-1] != NULL) {
             command[i] = strtok_r(NULL, " ", &saveptr);
             i = i + 1;
-        }
-        // Replace delimiter with NULL
-        command[i - 1] = NULL;
-
-        for (i = i - 1; i < 100; i++) {
-            command[i] = NULL;
-        }
-
-        for (i = 0; command[i] != NULL; i++) {
-            printf("TOKEN %s-%ld\n", command[i], strlen(command[i]));
         }
 
         // Create new process for the command.
@@ -42,15 +38,18 @@ int main(int argc, char *argv[]) {
         } else if (rc == 0) {
             int exec_code = execvp(command[0], command);
             if (exec_code < 0) {
-                fprintf(stderr, "EXEC_CODE %d\nERRNO %d\n", exec_code, errno);
                 fprintf(stderr, "Unable to run command.\n");
-                _exit(1);
+                _exit(EXIT_FAILURE);
             }
         } else {
             // Wait for child process to terminate.
             wait(NULL);
         }
-    }
 
+        // Empty buffer
+        for (i = 0; i < INPUT_LEN; i++) {
+            command[i] = NULL;
+        }
+    }
     return 0;
 }
