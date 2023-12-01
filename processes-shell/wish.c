c72b52548b22eb51fcfc6c172cef7b7b620c3a92
Ankit <ankithooda23@gmail.com>
2023-12-01 21:11:42 +0530

    Processes Shell

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -11,21 +11,24 @@
 
 
 // TODO: Revise this arbitrary limits.
-#define INPUT_LEN 1000
-#define PATH_LEN 100
-#define PATHNAME_LEN 150
+#define INPUT_LEN 10000               // Maximum input by user can be 10000 chars
+#define PATH_LEN 100                  // Path len can be 100 chars
+
+// TODO: Dynamically allocate this.
+#define COMMANDS 101                  // Maximum 100 Commands + 1 NUL Terminator
+#define COMMAND_TOKENS 101            // Maximum 100 tokens per are supported + 1 NUL Terminator
 
 // Static variable for storing path.
 char path[PATH_LEN];
 
 // Struct representing the parsed command.
-struct parsed_data {
-    char *command[INPUT_LEN];
-    short redirect;
+struct parsed_command {
+    char *command[COMMAND_TOKENS];
     char *redirect_file;
+    struct parsed_command *next;
 };
 
-void parse_input(char *, char *[], struct parsed_data *);
+struct parsed_data **parse_input(char *);
 void process(char *[], struct parsed_data *);
 
 int main(int argc, char *argv[]) {
@@ -35,11 +38,8 @@ int main(int argc, char *argv[]) {
     FILE *stream;               // File object to represent input file or stdin.
     size_t len = 0;
 
-    struct parsed_data *pd[];
-
     // Intialize path to the bin directory.
     strcpy(path, "/bin");
-
 
     // Set appropriate value for the input stream.
     if (argc == 2) {
@@ -53,18 +53,8 @@ int main(int argc, char *argv[]) {
         
         if (getline(&input, &len, stream) != -1) {
             if (strlen(input) > 1) {
-                // Before parsing we reset pd struct
-
-                pd.redirect = -1;
-                pd.redirect_file = NULL;
-
-                parse_input(input, command, &pd);
-                // int i = 0;
-                // while (command[i] != NULL) {
-                //     printf("%s\n", command[i]);
-                //     i = i + 1;
-                // }
-                process(command, &pd);
+                parse_input(input, pd);
+                // process(command, &pd);
             }
         } else {
             free(input);
@@ -75,119 +65,148 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void parse_input(char *input, char *command[], struct parsed_data *pd) {
+struct parsed_data ** parse_input(char *input) {
 
 
-    int i = 1;
-
-    char *saveptr;
-    char *temp;
-    char *temp1;
-
+    char *command, *command_args, *command_arg;
+    int command_index = 0;
+    int args_index = 0;
+    
     // Remove trailing newline
     input[strcspn(input, "\n")] = '\0';
 
     // Parse input for multiple commands with background directive.
 
-    temp = strtok_r(input, "&", &saveptr);
+    struct parsed_command *pc;
+    struct parsed_command *current;
 
-    while (temp != NULL) {
-        // Parse input for redirection.
-        temp1 = strtok_r(temp, ">", &saveptr);
 
-        pd->redirect_file = strtok_r(NULL, ">", &saveptr);
+    while (input != NULL) {
+        pc = malloc(sizeof(struct parsed_command));
+        current = pc;
 
-        if (pd->redirect_file != NULL) {
-            pd->redirect = 1;
+        command = strsep(&input, "&");
+        command_args = strsep(&command, ">");
+
+        pd[command_index].redirect_file = command;
+
+        while (command_args != NULL) {
+            command_arg = strsep(&command_args, " ");
+            pd[command_index].command[args_index] = command_arg;
+
+            args_index = args_index + 1;
         }
-
-        // Parse input into command and arguments.
-        command[0] = strtok_r(input, " ", &saveptr);
-
-        while (command[i-1] != NULL) {
-            command[i] = strtok_r(NULL, " ", &saveptr);
-            i = i + 1;
-        }
-    }    
-}
-
-void process(char *command[], struct parsed_data *pd) {
-
-    char pathname[PATHNAME_LEN];
-    strcpy(pathname, path);
-
-
-    // Built-in exit command
-    if (strcmp(command[0], "exit") == 0) {
-        if (command[1] != NULL) {
-            fprintf(stderr, "An error has occurred\n");
-            return;
-        } else {
-            _exit(EXIT_SUCCESS);
-        }
+        command_index = command_index + 1;
+            
     }
-
-    // Built-in cd command
-    if (strcmp(command[0], "cd") == 0) {
-        if (command[1] != NULL) {
-            if (chdir(command[1]) == -1) {
-                fprintf(stderr, "An error has occurred\n");
-            }
-        } else {
-            fprintf(stderr, "An error has occurred\n");
-        }
-        return;
-    }
-
-    // Built-in path command
-    if (strcmp(command[0], "path") == 0) {
-        // If path is invoked with 
-        if (command[1] == NULL) {
-            path[0] = '\0';
-        } else {
-            strcpy(path, command[1]);
-        }
-        return;
-    }
-
-    // Create a new process and replace it with user
-    // requested process.
-    int rc = fork();
-
-    if (rc < 0) {
-        fprintf(stderr, "Could not spawn process.");
-    } else if (rc == 0) {
-        // Get correct value of pathname
-        if (path == NULL) {
-            strcpy(pathname, command[0]);
-        } else {
-            if (path[strlen(path) - 1] != '/') {
-                strcat(pathname, "/");
-            }
-            strcat(pathname, command[0]);
-        }
-
-        // Before we replace the process image we 
-        // will open the pipes for redirection support.
         
-        if (pd->redirect == 1) {
-            close(STDOUT_FILENO);
-            close(STDERR_FILENO);
+        // printf("TOKEN %s\n", token);
+        // printf("INPUT %s\n", input);
+        // printf("------------------------------------->\n");
+        
+        // printf("SUBTOKEN %s\n", subtoken);
+        // printf("TOKEN %s\n", token);
+        // printf("#####################################\n");
+    
+    
+    
+    // while (token != NULL) {
+    //     // Each iteration parses a single command.
 
-            int stdout_fd = open(pd->redirect_file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-            int stderr_fd = open(pd->redirect_file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    //     // Parse input for redirection.
+    //     token = strtok_r(token, ">", &subtoken_saveptr);
+    //     *pd->redirect_file = strtok_r(NULL, ">", &subtoken_saveptr);
+    //     if (pd->redirect_file != NULL) {
+    //         pd->redirect = 1;
+    //     }
 
-            if (stdout_fd < 0 || stderr_fd < 0) {
-                fprintf(stderr, "An error has occurred");
-            }
-        }
+    //     // Parse input into command and arguments.
+    //     pd[j]->command[i] = strtok_r(token, " ", &subtoken_saveptr);
 
-        int exec_code = execv(pathname, command);
-        if (exec_code < 0) {
-            fprintf(stderr, "An error has occurred\n");
-            _exit(EXIT_FAILURE);
-        }
-    } else {
-        wait(NULL);
-    }
+    //     while (pd[0]->command[i-1] != NULL) {
+    //         pd[0]->command[i] = strtok_r(NULL, " ", &saveptr);
+    //         i = i + 1;
+    //     }
+    // }    
 }
+
+// void process(char *command[], struct parsed_data **pd) {
+
+//     char pathname[PATH_LEN];
+//     strcpy(pathname, path);
+
+
+//     // Built-in exit command
+//     if (strcmp(command[0], "exit") == 0) {
+//         if (command[1] != NULL) {
+//             fprintf(stderr, "An error has occurred\n");
+//             return;
+//         } else {
+//             _exit(EXIT_SUCCESS);
+//         }
+//     }
+
+//     // Built-in cd command
+//     if (strcmp(command[0], "cd") == 0) {
+//         if (command[1] != NULL) {
+//             if (chdir(command[1]) == -1) {
+//                 fprintf(stderr, "An error has occurred\n");
+//             }
+//         } else {
+//             fprintf(stderr, "An error has occurred\n");
+//         }
+//         return;
+//     }
+
+//     // Built-in path command
+//     if (strcmp(command[0], "path") == 0) {
+//         // If path is invoked with 
+//         if (command[1] == NULL) {
+//             path[0] = '\0';
+//         } else {
+//             strcpy(path, command[1]);
+//         }
+//         return;
+//     }
+
+//     // Create a new process and replace it with user
+//     // requested process.
+//     int rc = fork();
+
+//     if (rc < 0) {
+//         fprintf(stderr, "Could not spawn process.");
+//     } else if (rc == 0) {
+//         // Get correct value of pathname
+//         if (path == NULL) {
+//             strcpy(pathname, command[0]);
+//         } else {
+//             if (path[strlen(path) - 1] != '/') {
+//                 strcat(pathname, "/");
+//             }
+//             strcat(pathname, command[0]);
+//         }
+
+//         // Before we replace the process image we 
+//         // will open the pipes for redirection support.
+        
+//         if (pd->redirect == 1) {
+//             close(STDOUT_FILENO);
+//             close(STDERR_FILENO);
+
+//             int stdout_fd = open(pd->redirect_file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+//             int stderr_fd = open(pd->redirect_file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+//             if (stdout_fd < 0 || stderr_fd < 0) {
+//                 fprintf(stderr, "An error has occurred");
+//             }
+//         }
+
+//         int exec_code = execv(pathname, command);
+//         if (exec_code < 0) {
+//             fprintf(stderr, "An error has occurred\n");
+//             _exit(EXIT_FAILURE);
+//         }
+//     } else {
+//         wait(NULL);
+//     }
+// }
