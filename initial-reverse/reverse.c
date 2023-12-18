a3b4436710c3d71cd7bfa35cc222003419e511c0
Ankit <ankithooda23@gmail.com>
2023-12-18 21:59:48 +0530

    Added support for increasing buffer to store any number of lines

--- initial-reverse/reverse.c
+++ initial-reverse/reverse.c
@@ -3,18 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#define MAX_LINES 1000
+#define INITIAL_BUFFER_LEN 2
 
 char *usage_message = "usage: reverse <input> <output>";
 char *file_error_template = "error: cannot open file '%s'\n";
 char *file_same_error_message = "Input and output file must differ";
+char *malloc_failed_message = "malloc failed";
 
 int main(int argc, char **argv) {
 
   FILE *input, *output;
-  char *line[MAX_LINES];                     // Allocate a buffer for storing pointers to the lines.
+  char **lines = NULL;            // Allocate a buffer for storing pointers to the lines.
+  char **temp_buffer;
   size_t line_len;
-  long line_num = 0;
+  size_t line_num = 0;
+  size_t buffer_len = INITIAL_BUFFER_LEN;
+
 
   // No Input or Output files are given.
   if (argc == 1) {
@@ -27,7 +31,7 @@ int main(int argc, char **argv) {
 
   } else if (argc == 3) {
     // Before we open the input and output file
-    // we will checkif they are both the same file.
+    // we will check if they are both the same file.
     if (strcmp(argv[1], argv[2]) == 0) {
       fprintf(stderr, "%s\n", file_same_error_message);
       exit(EXIT_FAILURE);
@@ -50,18 +54,36 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   }
 
+  // Allocate initial buffer
+  lines = reallocarray(lines, buffer_len, sizeof(char *));
+
   // Getline from the input
   // store the pointers to each line in a buffer.
-  while (getline(&line[line_num], &line_len, input) != -1 && line_num < MAX_LINES) {
+  while ((line_num < buffer_len) && (getline(&lines[line_num], &line_len, input) != -1)) {
     line_num++;
+    // If we have more lines than the current buffer_len
+    // we double the buffer size;
+    if (line_num >= buffer_len) {
+      temp_buffer = lines;
+      lines = reallocarray(lines, buffer_len * 2, sizeof(char *));
+      if (lines == NULL) {
+        fprintf(stderr, "%s\n", malloc_failed_message);
+        exit(EXIT_FAILURE);
+      }
+      memcpy(lines, temp_buffer, buffer_len * sizeof(char *));
+
+      buffer_len = buffer_len * 2;
+      free(temp_buffer);
+    }
   }
 
   // Write the lines stored in the buffer
   // to the output file in reverse order.
-  while(line_num >= 0) {
-    fprintf(output, "%s", line[line_num]);
+  while (line_num >= 0) {
+    fprintf(output, "%s", lines[line_num]);
     line_num--;
   }
+
 
   fclose(input);
   fclose(output);
