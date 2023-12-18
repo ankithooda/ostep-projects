bd9191b038caef99a86ff83320bfe6856ad3aa5f
Ankit <ankithooda23@gmail.com>
2023-12-18 14:18:50 +0530

    Implemented reverse functionality

--- initial-reverse/reverse.c
+++ initial-reverse/reverse.c
@@ -1,45 +1,66 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#define MAX_LINES 1000
 
 char *usage_message = "usage: reverse <input> <output>";
-int main(int argc, char **argv) {
-  printf("Hello World!\n");
-  printf("No. of arguments %d\n", argc);
+char *file_error_template = "error: cannot open file '%s'\n";
+char *file_same_error_message = "Input and output file must differ";
 
-  for (int i = 0; i < argc; i++) {
-    printf("%s\n", argv[i]);
-  }
+int main(int argc, char **argv) {
 
   FILE *input, *output;
-  char *line;
+  char *line[MAX_LINES];                     // Allocate a buffer for storing pointers to the lines.
   size_t line_len;
+  long line_num = 0;
+
   // No Input or Output files are given.
   if (argc == 1) {
     input = stdin;
     output = stdout;
+
   } else if (argc == 2) {
     input = fopen(argv[1], "r");
+    output = stdout;
+
   } else if (argc == 3) {
+    // Before we open the input and output file
+    // we will checkif they are both the same file.
+    if (strcmp(argv[1], argv[2]) == 0) {
+      fprintf(stderr, "%s\n", file_same_error_message);
+      exit(EXIT_FAILURE);
+    }
     input = fopen(argv[1], "r");
     output = fopen(argv[2], "w");
+
   } else {
     fprintf(stderr, "%s\n", usage_message);
+    exit(EXIT_FAILURE);
   }
 
   if (input == NULL) {
-    fprintf(stderr, "error: cannot open file '%s'\n", argv[1]);
+    fprintf(stderr, file_error_template, argv[1]);
     exit(EXIT_FAILURE);
   }
 
   if (output == NULL) {
-    fprintf(stderr, "error: cannot open file '%s'\n", argv[2]);
+    fprintf(stderr, file_error_template, argv[2]);
     exit(EXIT_FAILURE);
   }
 
-  while (getline(&line, &line_len, input) != -1) {
-    printf("line_len %zu\n", line_len);
-    fprintf(output, "%s", line);
+  // Getline from the input
+  // store the pointers to each line in a buffer.
+  while (getline(&line[line_num], &line_len, input) != -1 && line_num < MAX_LINES) {
+    line_num++;
+  }
+
+  // Write the lines stored in the buffer
+  // to the output file in reverse order.
+  while(line_num >= 0) {
+    fprintf(output, "%s", line[line_num]);
+    line_num--;
   }
 
   fclose(input);
