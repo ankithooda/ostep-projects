66c64ab773c8c2c279c43f26c27eb107d1142474
Ankit <ankithooda23@gmail.com>
2024-04-24 11:47:34 +0530

    Added boiler plate for testing concurrent q

--- concurrency-ds/concurrent_q.c
+++ concurrency-ds/concurrent_q.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <assert.h>
+
+#define MAX_THREADS 8
+#define MILLION 10
 
 typedef struct __node_t {
-  int key;
-  struct __node *next;
+  int value;
+  struct __node_t *next;
 } node_t;
 
 // Queue runs from head->tail.
@@ -17,6 +21,8 @@ typedef struct __queue {
   pthread_mutex_t tail_lock;
 } queue_t;
 
+queue_t data;
+
 void queue_init(queue_t *q) {
   q->head = NULL;
   q->tail = NULL;
@@ -25,14 +31,14 @@ void queue_init(queue_t *q) {
 }
 
 // enqueue adds an element at the end of the list.
-int enqueue(queue_t *q, int key) {
+int enqueue(queue_t *q, int value) {
   node_t *new = malloc(sizeof(node_t));
 
   if (new == NULL) {
     perror("Could not allocate memory\n");
     return -1;
   } else {
-    new->key = key;
+    new->value = value;
     new->next = NULL;
 
     pthread_mutex_lock(&q->tail_lock);
@@ -52,17 +58,40 @@ int enqueue(queue_t *q, int key) {
 }
 
 node_t *dequeue(queue_t *q) {
-  node_t n;
+  node_t *n;
   pthread_mutex_lock(&q->head_lock);
   if (q->head == NULL) {
-    return -1;
+    return NULL;
   } else {
     n = q->head;
     q->head = q->head->next;
   }
   pthread_mutex_unlock(&q->head_lock);
+  return n;
+}
+
+void *task(void *arg) {
+  for (int i = 0; i < MILLION; i++) {
+    if (i % 2 == 0) {
+      enqueue(&data, i);
+    } else {
+      dequeue(&data);
+    }
+  }
+  return NULL;
 }
 
 int main() {
+  pthread_t threads[MAX_THREADS];
+  queue_init(&data);
+
+  for (int i = 0; i < MAX_THREADS; i++) {
+    assert(pthread_create(&threads[i], NULL, task, NULL) == 0);
+  }
+
+  for (int i = 0; i < MAX_THREADS; i++) {
+    assert(pthread_join(threads[i], NULL) == 0);
+  }
+
   return 0;
 }
